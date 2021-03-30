ee8a82f5b18b0a709e0e1f9e5545a86a932888b8
Leghnider <zineb.leghnider@icloud.com>
2021-03-30 14:07:59 +0100

    test env created

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -7,7 +8,7 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :'bookmarks'
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $0
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,7 +1,9 @@
-feature 'viewing bookmarks' do
-  scenario 'when visiting /bookmarks it shows a list of bookmarks' do
-    visit('/bookmarks')
-    expect(page).to have_content 'https://www.google.co.uk'
-    expect(page).to have_content 'https://www.bbc.co.uk'
-  end
-end
+# feature 'viewing bookmarks' do
+#   scenario 'when visiting /bookmarks it shows a list of bookmarks' do
+#     visit('/bookmarks')
+
+#     expect(page).to have_content 'http://www.makersacademy.com'
+#     expect(page).to have_content 'http://www.askjeeves.com'
+#     expect(page).to have_content 'http://www.google.com'
+#   end
+# end
